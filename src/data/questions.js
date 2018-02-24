edceb7d4f9068462b519aaf8d61827b37674c5a2
root <root@LAPTOP-95BDLGUR.localdomain>
2018-02-24 08:42:52 -0500

    start questions.js

---
+++ src/data/questions.js
@@ -0,0 +1,20 @@
+const questions = {
+    "Employee Resource Groups" : {
+        1: {
+            q : "Does your organization have an LGBTQ2+ employee resource group (ERG) in Canada? Select all that apply.",
+            a1: "Yes, and the group has a defined role and formal recognition",
+            a2: "Yes, and the group has access to a budget and other resources",
+            a3: "Yes, we do have a group but it is not given formal recognition or support.",
+            a4: "No, we do not have a group, but we have a formal agreement with external support networks in our region or area of work",
+            a5: "No, we do not have a group, but we make other facilities and resources available to LGBTQ2+ employees for networking, consulting, and providing feedback to the organization",
+            a6: "No, we do not have a group but we are in the process of setting one up",
+            a7: "No, we do not have a group nor do we offer specific alternative resources",
+            onlyOneAllowed: "false"
+        },
+        2: {
+
+        },
+    }
+}
+
+export default questions;
